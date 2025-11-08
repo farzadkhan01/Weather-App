8a7f771a57f0d0e08c4b3bf1ce59afd2784327fb
Farzad khan <khanfarzd559@gmail.com>
2025-11-08 15:27:42 +0500

    progress

--- src/JS/script.js
+++ src/JS/script.js
@@ -4,8 +4,9 @@ class appBluePrint {
 
     constructor() {
         this.coords = null;
-        this.placeName = null;
-
+        this.weatherData = {
+        };
+        this.currentDate()
 
     }
 
@@ -28,16 +29,95 @@ class appBluePrint {
 
             const locationInfo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
             const data = await locationInfo.json()
-            this.placeName = (data.city + data.countryName).toLowerCase()
+            this.cityAndCountry = data;
+            console.log(data)
+            this.weatherData.apiPlaceName = (data.city + data.countryName).toLowerCase()
+            this.weatherData.placeName = (data.city + ', ' + data.countryName)
         } catch (err) {
             console.error('Error From Coords Convertor: ', err.message);
         }
     }
 
+    currentDate() {
+        const weekDays = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thirsday',
+            'Friday',
+            'Saturday'
+        ]
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ]
+        const date = new Date();
+        const day = weekDays[date.getDay()]
+        this.weatherData.day = day;
+        this.weatherData.date = `${date.getDate()} ${months[date.getMonth()]}, ${date.getFullYear()} `;
+    }
+
     async locationData() {
-        const countryInfo = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.placeName}?unitGroup=us&include=days&key=${LOCATION__API__KEY}&contentType=json`)
+        const countryInfo = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.weatherData.apiPlaceName}?unitGroup=us&include=days&key=${LOCATION__API__KEY}&contentType=json`)
         const data = await countryInfo.json()
-        console.log(data)
+        this.weatherData.TotalData = data;
+
+        this.weatherData.temp = Math.trunc(this.weatherData.TotalData.days[0].temp)
+        this.weatherData.tempMax = Math.trunc(this.weatherData.TotalData.days[0].tempmax)
+        this.weatherData.tempMin = Math.trunc(this.weatherData.TotalData.days[0].tempmin)
+        this.weatherData.feelsLike = Math.trunc(this.weatherData.TotalData.days[0].temp)
+
+
+
+
+        console.log(this.weatherData)
+    }
+
+    async currentDay() {
+        const html = `
+            <div class="d-block d-xl-flex align-items-center justify-content-between">
+                <div class="today-weather ">
+                    <div class="img-review">
+                        <div class="location d-flex align-items-center justify-content-start rounded-pill">
+                            <i class="fa-solid fa-location-dot"></i>
+                            <h6 class="current__location m-0">${this.weatherData.placeName}</h6>
+                        </div>
+                        <div class="feels-like">
+                            <img src="src/Imges/cloud.png" alt="Cloud">
+                        </div>
+                    </div>
+                    <div class="today-info">
+                        <div class="info-content">
+                            <h3 class="today-day">${this.weatherData.day}</h3>
+                            <h5 class="today-date">${this.weatherData.date}</h3>
+                        </div>
+                        <div class="info-content">
+                            <h3 class="today-temp">${Math.trunc(this.weatherData.TotalData.days[0].temp)}</h3>
+                            <h5 class="today-date">High:  Low: 10</h3>
+                        </div>
+                        <div class="info-content">
+                            <h3 class="today-temp">26°C</h3>
+                            <h5 class="today-date">Feels Like</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="feels-like displaying">
+                    <img src="src/Imges/cloud.png" alt="Cloud">
+                </div>
+            </div>
+        `;
+        // console.log(html)
     }
 
     async init() {
@@ -45,6 +125,7 @@ class appBluePrint {
             await this.getCurrentLocation();
             await this.coordinatesToLocationName();
             await this.locationData()
+            await this.currentDay() // there is an HTML elements which loaded first then before i want this to run after the upperfunction runs
         } catch (err) {
             console.error(err)
         }
