775399c8a4f5c9b21fedbbbb981ef5ace99b715a
Farzad khan <khanfarzd559@gmail.com>
2025-11-10 15:38:35 +0500

    Inserting Data

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,12 +1,12 @@
-const LOCATION__API__KEY = 'QXSS9XCRCFTRTLPNRT53LKY6T'
+const LOCATION__API__KEY = 'QXSS9XCRCFTRTLPNRT53LKY6T';
 
 class appBluePrint {
+    #parentEl;
 
     constructor() {
         this.coords = null;
-        this.weatherData = {
-        };
-        this.currentDate()
+        this.weatherData = {};
+        this.#currentDate()
 
     }
 
@@ -30,15 +30,16 @@ class appBluePrint {
             const locationInfo = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
             const data = await locationInfo.json()
             this.cityAndCountry = data;
-            console.log(data)
-            this.weatherData.apiPlaceName = (data.city + data.countryName).toLowerCase()
-            this.weatherData.placeName = (data.city + ', ' + data.countryName)
+
+            this.weatherData.apiPlaceName = (data.city + data.countryName).toLowerCase();
+            this.weatherData.placeName = (data.city + ', ' + data.countryName);
+
         } catch (err) {
             console.error('Error From Coords Convertor: ', err.message);
         }
     }
 
-    currentDate() {
+    #currentDate() {
         const weekDays = [
             'Sunday',
             'Monday',
@@ -68,23 +69,34 @@ class appBluePrint {
         this.weatherData.date = `${date.getDate()} ${months[date.getMonth()]}, ${date.getFullYear()} `;
     }
 
+    /**
+     * @author Farzad Khan
+     * @summary This Func gets data and then saves all the data with proper formate in an object called weatherData
+     * @DataFetchedFrom weather.visualcrossing.com
+     */
     async locationData() {
         const countryInfo = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${this.weatherData.apiPlaceName}?unitGroup=us&include=days&key=${LOCATION__API__KEY}&contentType=json`)
         const data = await countryInfo.json()
-        this.weatherData.TotalData = data;
-
-        this.weatherData.temp = Math.trunc(this.weatherData.TotalData.days[0].temp)
-        this.weatherData.tempMax = Math.trunc(this.weatherData.TotalData.days[0].tempmax)
-        this.weatherData.tempMin = Math.trunc(this.weatherData.TotalData.days[0].tempmin)
-        this.weatherData.feelsLike = Math.trunc(this.weatherData.TotalData.days[0].temp)
 
 
-
-
+        this.weatherData.weekdays = data.days
+        this.weatherData.temp = Math.trunc(data.days[0].temp)
+        this.weatherData.tempMax = Math.trunc((data.days[0].tempmax - 32) * 5 / 9) // (32°F − 32) × 5/9 = 0°C
+        this.weatherData.tempMin = Math.trunc((data.days[0].tempmin - 32) * 5 / 9) // (32°F − 32) × 5/9 = 0°C
+        this.weatherData.humidity = Math.trunc((data.days[0].humidity - 32) * 5 / 9)
+        console.log(this.weatherData.tempMax, this.weatherData.tempMin, this.weatherData.humidity)
+        // Data of section (Weekdays)
+        this.weatherData.day1 = this.daysManagement(1)
+        this.weatherData.day2 = this.daysManagement(2)
+        this.weatherData.day3 = this.daysManagement(3)
+        this.weatherData.day4 = this.daysManagement(4)
+        this.weatherData.day5 = this.daysManagement(5)
+        this.weatherData.day6 = this.daysManagement(6)
         console.log(this.weatherData)
+
     }
 
-    async currentDay() {
+    async todayData() {
         const html = `
             <div class="d-block d-xl-flex align-items-center justify-content-between">
                 <div class="today-weather ">
@@ -103,29 +115,83 @@ class appBluePrint {
                             <h5 class="today-date">${this.weatherData.date}</h3>
                         </div>
                         <div class="info-content">
-                            <h3 class="today-temp">${Math.trunc(this.weatherData.TotalData.days[0].temp)}</h3>
-                            <h5 class="today-date">High:  Low: 10</h3>
+                            <h3 class="today-temp">${Math.trunc((this.weatherData.weekdays[0].temp - 32) * 5 / 9)}°C</h3>
+                            <h5 class="today-date">High: ${this.weatherData.tempMax} Low: ${this.weatherData.tempMin}</h3>
                         </div>
                         <div class="info-content">
-                            <h3 class="today-temp">26°C</h3>
-                            <h5 class="today-date">Feels Like</h3>
+                            <h3 class="today-temp">${this.weatherData.humidity}°C</h3>
+                            <h5 class="today-date">Humidity</h3>
                         </div>
                     </div>
                 </div>
-                <div class="feels-like displaying">
+                <!--<div class="feels-like displaying">
                     <img src="src/Imges/cloud.png" alt="Cloud">
-                </div>
+                </div>-->
             </div>
         `;
-        // console.log(html)
+        this.#parentEl = document.querySelector('#current__city')
+        this.#parentEl.innerHTML = ''
+
+        this.#parentEl.insertAdjacentHTML('afterbegin', html);
+    }
+
+    daysManagement(num, variable) {
+        const { date, temp } = { date: this.weatherData.weekdays[num].datetime, temp: this.weatherData.weekdays[num].temp }
+        if (variable === 'date')
+            return date.slice(-5)
+        if (variable === 'temp')
+            return temp
+    }
+
+    async weekDays() {
+        const html = `
+            <div class="hour-card">
+                <h3>${this.daysManagement(1, 'date')}</h3>
+                <img src="src/Imges/cloud.png" alt="">
+                <h3>${this.daysManagement(1, 'temp')}°C</h3>
+            </div>
+            <div class="hour-card">
+                <h3>${this.daysManagement(2, 'date')}</h3>
+                <img src="src/Imges/cloud.png" alt="">
+                <h3>${this.daysManagement(2, 'temp')}°C</h3>
+            </div>
+            <div class="hour-card">
+                <h3>${this.daysManagement(3, 'date')}</h3>
+                <img src="src/Imges/cloud.png" alt="">
+                <h3>${this.daysManagement(3, 'temp')}°C</h3>
+            </div>
+            <div class="hour-card">
+                <h3>${this.daysManagement(4, 'date')}</h3>
+                <img src="src/Imges/cloud.png" alt="">
+                <h3>${this.daysManagement(4, 'temp')}°C</h3>
+            </div>
+            <div class="hour-card">
+                <h3>${this.daysManagement(5, 'date')}</h3>
+                <img src="src/Imges/cloud.png" alt="">
+                <h3>${this.daysManagement(5, 'temp')}°C</h3>
+            </div>
+            <div class="hour-card">
+                <h3>${this.daysManagement(6, 'date')}</h3>
+                <img src="src/Imges/cloud.png" alt="">
+                <h3>${this.daysManagement(6, 'temp')}°C</h3>
+            </div>
+        `;
+        console.log(html)
+        this.#parentEl = document.querySelector('.hourly')
+        this.#parentEl.innerHTML = ''
+
+        this.#parentEl.insertAdjacentHTML('beforeend', html);
+
     }
 
     async init() {
         try {
             await this.getCurrentLocation();
             await this.coordinatesToLocationName();
-            await this.locationData()
-            await this.currentDay() // there is an HTML elements which loaded first then before i want this to run after the upperfunction runs
+            await this.locationData();
+            this.todayData();
+            this.weekDays();
+            console.log(this.daysManagement(1, 'date'))
         } catch (err) {
             console.error(err)
         }
